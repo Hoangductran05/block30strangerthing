010705bfc85f0525f6ddf0b1f2bf642d7b20e6be
Hoangductran05 <hoangducdoodle@gmail.com>
2023-09-28 09:22:56 -0500

    login

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,5 +1,23 @@
+import React, { useState } from "react"
+
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
     return(
-        <h1>Login</h1>
+        <>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email" >Email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com " id="email" name="email" />
+            <label htmlFor="password" >Password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password" />
+            <button type="submit">Log in</button>
+        </form>
+        <button>Already have an account? Register here.</button>
+        </>
     )
 }
