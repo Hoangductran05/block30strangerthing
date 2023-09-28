349d4304639dcd5db23fe11f4216fe6fd631e368
Hoangductran05 <hoangducdoodle@gmail.com>
2023-09-28 09:27:25 -0500

    login

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,23 +1,87 @@
-import React, { useState } from "react"
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Login() {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password == '') {
+      setError('Please enter correct details!');
+    } else {
+      // TODO: Send signup request to server
+      setError('Login successful');
+      setEmail('');
+      setPassword('');
     }
-    return(
-        <>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="email" >Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com " id="email" name="email" />
-            <label htmlFor="password" >Password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password" />
-            <button type="submit">Log in</button>
-        </form>
-        <button>Already have an account? Register here.</button>
-        </>
-    )
-}
+  };
+
+  return (
+    <section className="login-wrapper p-5">
+      <div className="container-xxl">
+        <div className="row justify-content-center">
+          <div className="col-lg-4 col-md-8 col-sm-10">
+              <div className="card">
+                <div className="card-body p-5">
+                  <h2 className="text-center">LOGIN</h2>
+                  <p className="text-center mb-4">Welcome Back!!</p>
+                  <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                      <label htmlFor="email" className="form-label mb-3">
+                        Enter Your Email address
+                      </label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        placeholder="enter email here ..."
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label htmlFor="password" className="form-label mb-3">
+                        Enter Your password
+                      </label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="enter password here..."
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        required
+                      />
+                    </div>
+                    {error && (
+                      <div className="alert alert-danger">{error}</div>
+                    )}
+                    <div className="mb-3">
+                      <Link to="/forgotpasword" className="form-link">
+                        Forgot password?
+                      </Link>
+                    </div>
+                    <div className="d-flex justify-content-between align-items-center mb-3">
+                      <p className='m-0'>Dont have an account?</p>
+                      <Link to="/register" className="form-link">
+                        Sign up
+                      </Link>
+                    </div>
+                    <div className="d-grid gap-2">
+                      <button type="submit">Login</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
